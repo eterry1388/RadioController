a53da1ff7130b121b9b723d35a7b18ea737b5377
Eric Terry <eterry1388@aol.com>
2021-01-20 16:55:38 -0700

    WIP with websockets

--- RadioController/ViewController.swift
+++ RadioController/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import CoreMotion
+import Starscream
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WebSocketDelegate {
+    var socket: WebSocket!
+    var isConnected = false
+    let server = WebSocketServer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var request = URLRequest(url: URL(string: "http://192.168.1.18:8080")!)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket.delegate = self
+        socket.connect()
+        
+        startAccelerometers()
     }
+    
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+        switch event {
+        case .connected(let headers):
+            isConnected = true
+            print("websocket is connected: \(headers)")
+        case .disconnected(let reason, let code):
+            isConnected = false
+            print("websocket is disconnected: \(reason) with code: \(code)")
+        case .text(let string):
+            print("Received text: \(string)")
+        case .binary(let data):
+            print("Received data: \(data.count)")
+        case .ping(_):
+            break
+        case .pong(_):
+            break
+        case .viabilityChanged(_):
+            break
+        case .reconnectSuggested(_):
+            break
+        case .cancelled:
+            isConnected = false
+        case .error(let error):
+            isConnected = false
+            handleError(error)
+        }
+    }
+    
+    func handleError(_ error: Error?) {
+        if let e = error as? WSError {
+            print("websocket encountered an error: \(e.message)")
+        } else if let e = error {
+            print("websocket encountered an error: \(e.localizedDescription)")
+        } else {
+            print("websocket encountered an error")
+        }
+    }
+    
+    let motion = CMMotionManager()
+    var timer = Timer()
+    
+    func startAccelerometers() {
+       // Make sure the accelerometer hardware is available.
+       if self.motion.isAccelerometerAvailable {
+          self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+          self.motion.startAccelerometerUpdates()
 
+          // Configure a timer to fetch the data.
+          self.timer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: { (timer) in
+             // Get the accelerometer data.
+             if let data = self.motion.accelerometerData {
+                let x = data.acceleration.x
+                let y = data.acceleration.y
+                let z = data.acceleration.z
 
+                // Use the accelerometer data in your app.
+                print("X: \(x), Y: \(y), Z: \(z)")
+                self.socket.write(string: "X: \(x), Y: \(y), Z: \(z)")
+             }
+          })
+
+          // Add the timer to the current run loop.
+        RunLoop.current.add(self.timer, forMode: RunLoop.Mode.default)
+       }
+    }
 }
-
